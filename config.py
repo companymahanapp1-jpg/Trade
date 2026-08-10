ecfc8bebfbbccc402d1768c80f302689e0921e69
companymahanapp1-jpg <company.mahan.app1@gmail.com>
2026-08-10 16:43:17 +0330

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
-DEFAULT_COOLDOWN = int(os.getenv("DEFAULT_COOLDOWN", "600"))
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN is not configured")

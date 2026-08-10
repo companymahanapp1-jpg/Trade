6377bd271d6a3ef2492a0c76d7156a79c2e40785
companymahanapp1-jpg <company.mahan.app1@gmail.com>
2026-08-10 17:11:09 +0330

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-BOT_TOKEN=os.getenv("BOT_TOKEN","")
-OWNER_ID=int(os.getenv("OWNER_ID","0"))
-START_EMOJI_ID=os.getenv("START_EMOJI_ID","")
-SUPPORT_EMOJI_ID=os.getenv("SUPPORT_EMOJI_ID","")
-ADMIN_EMOJI_ID=os.getenv("ADMIN_EMOJI_ID","")
-if not BOT_TOKEN: raise RuntimeError("BOT_TOKEN is missing")

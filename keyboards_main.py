102cdbf81a24d95944300e81e4ce4c532db8ba3f
companymahanapp1-jpg <company.mahan.app1@gmail.com>
2026-08-10 16:44:25 +0330

    Delete keyboards_main.py

--- keyboards_main.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-def main_keyboard(is_admin=False):
-    rows = [[KeyboardButton(text="🎓 شروع یادگیری"), KeyboardButton(text="💬 پشتیبانی")]]
-    if is_admin: rows.append([KeyboardButton(text="👑 پنل مدیریت")])
-    return ReplyKeyboardMarkup(keyboard=rows, resize_keyboard=True, is_persistent=True)
